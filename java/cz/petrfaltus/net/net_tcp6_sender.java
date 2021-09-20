ad0f8098d5ada7a2c56bc587af700ddd57947d22
Petr Faltus <petrfaltus@petrfaltus.net>
2021-09-20 20:28:09 +0200

    Java - TCP IPv6 sender - create the empty console application

---
+++ java/cz/petrfaltus/net/net_tcp6_sender.java
@@ -0,0 +1,11 @@
+
+package cz.petrfaltus.net;
+
+import static java.lang.System.out;
+
+public class net_tcp6_sender {
+
+    public static void main(String[] args) {
+        out.println("OK");
+    }
+}
