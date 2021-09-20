cdeec0b45f084f85b3182535665bd351e30ba134
Petr Faltus <petrfaltus@petrfaltus.net>
2021-09-20 19:56:51 +0200

    Java - TCP IPv4 receiver - create the empty console application

---
+++ java/cz/petrfaltus/net/net_tcp_receiver.java
@@ -0,0 +1,11 @@
+
+package cz.petrfaltus.net;
+
+import static java.lang.System.out;
+
+public class net_tcp_receiver {
+
+    public static void main(String[] args) {
+        out.println("OK");
+    }
+}
