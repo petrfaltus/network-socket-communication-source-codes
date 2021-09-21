3f71f4edd0ed600c38d8d178e1af65dc9d922e49
Petr Faltus <petrfaltus@petrfaltus.net>
2021-09-21 11:07:08 +0200

    C# - UDP IPv4 receiver - create the empty console application

---
+++ csharp/src/NetUDPreceiver.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace NetUDPreceiver
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("OK");
+        }
+    }
+}
