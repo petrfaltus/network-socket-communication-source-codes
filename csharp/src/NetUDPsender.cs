dc832663ceb8cb7a74021c61c780fb63a3074421
Petr Faltus <petrfaltus@petrfaltus.net>
2021-09-21 10:37:30 +0200

    C# - UDP IPv4 sender - create the empty console application

---
+++ csharp/src/NetUDPsender.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace NetUDPsender
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("OK");
+        }
+    }
+}
