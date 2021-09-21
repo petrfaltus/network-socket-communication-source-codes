5ef626205ddc598f824a4a536cf510f5db392654
Petr Faltus <petrfaltus@petrfaltus.net>
2021-09-21 11:54:33 +0200

    C# - UDP IPv6 receiver - create the empty console application

---
+++ csharp/src/NetUDP6receiver.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace NetUDP6receiver
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("OK");
+        }
+    }
+}
