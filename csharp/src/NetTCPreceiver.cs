1c42f4773db063ee55b3a62b4b4249ac41d9ddb7
Petr Faltus <petrfaltus@petrfaltus.net>
2021-09-21 09:13:41 +0200

    C# - TCP IPv4 receiver - create the empty console application

---
+++ csharp/src/NetTCPreceiver.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace NetTCPreceiver
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("OK");
+        }
+    }
+}
