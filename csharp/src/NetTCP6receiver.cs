c3d5ec766915cd0f970af6686d9363554ff5eba9
Petr Faltus <petrfaltus@petrfaltus.net>
2021-09-21 10:20:21 +0200

    C# - TCP IPv6 receiver - create the empty console application

---
+++ csharp/src/NetTCP6receiver.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace NetTCP6receiver
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("OK");
+        }
+    }
+}
