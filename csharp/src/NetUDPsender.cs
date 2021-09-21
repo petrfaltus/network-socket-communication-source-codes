7798edf8dd3111fc6d72c7a217a6cbc10abfb766
Petr Faltus <petrfaltus@petrfaltus.net>
2021-09-21 11:05:42 +0200

    C# - UDP IPv4 sender - implement

--- csharp/src/NetUDPsender.cs
+++ csharp/src/NetUDPsender.cs
@@ -1,13 +1,48 @@
 using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
 
 namespace NetUDPsender
 {
     public class Program
     {
+        private static readonly string RECEIVER_ADDRESS = "127.0.0.1";
+        private static readonly int RECEIVER_PORT = 10000;
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("OK");
+            Console.WriteLine("UDP IPv4 datagram socket sender");
+
+            try
+            {
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                Console.WriteLine("- socket created");
+
+                string msg;
+                if (args.Length > 0)
+                    msg = args[0]; // message is the first parameter, for example "stop" to stop the receiver
+                else
+                {
+                    DateTime now = DateTime.Now;
+                    msg = string.Format("This is C# message sent at {0} in {1}", now.ToString("d.M.yyyy"), now.ToString("H:mm:ss"));
+                }
+                Console.WriteLine("|{0}|", msg);
+
+                byte[] buffer = Encoding.ASCII.GetBytes(msg);
+                IPAddress receiver_address = IPAddress.Parse(RECEIVER_ADDRESS);
+                IPEndPoint receiver = new IPEndPoint(receiver_address, RECEIVER_PORT);
+
+                int sent_length = socket.SendTo(buffer, receiver);
+                Console.WriteLine("- message {0}B of {1}B sent to {2}:{3}", sent_length, msg.Length, receiver_address, RECEIVER_PORT);
+
+                socket.Close();
+                Console.WriteLine("- socket closed");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("- {0}", ex.Message);
+            }
         }
     }
 }
