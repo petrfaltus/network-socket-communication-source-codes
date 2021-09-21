074690dd65c895dbdfada670e8aed721bad31912
Petr Faltus <petrfaltus@petrfaltus.net>
2021-09-21 09:58:57 +0200

    C# - TCP IPv6 sender - create the empty console application

---
+++ csharp/01-compile-run-tcp6-sender.cmd
@@ -0,0 +1,15 @@
+@echo off
+
+set SRC=src
+set OUT=bin
+
+set PROJECT=NetTCP6sender
+set SOURCE=%SRC%\%PROJECT%.cs
+set EXECUTABLE=%OUT%\%PROJECT%.exe
+set ICON=ico\NetCommunication.ico
+set DOTNET_HOME=C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319
+
+mkdir %OUT%
+"%DOTNET_HOME%\csc.exe" /win32icon:%ICON% /out:%EXECUTABLE% %SOURCE%
+
+%EXECUTABLE% %1
---
+++ csharp/src/NetTCP6sender.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace NetTCP6sender
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("OK");
+        }
+    }
+}
