18d7c9566a83206ca351139709ab1375932a19e8
Petr Faltus <petrfaltus@petrfaltus.net>
2021-09-22 09:40:47 +0200

    PHP - TCP IPv4 sender - implement

--- php/01-run-tcp4-sender.cmd
+++ php/01-run-tcp4-sender.cmd
@@ -5,4 +5,4 @@ set SRC=src
 set PROJECT=net_tcp_sender
 set SOURCE=%SRC%\%PROJECT%.php
 
-"%PHP_HOME%\php.exe" %SOURCE%
+"%PHP_HOME%\php.exe" %SOURCE% %1
--- php/src/net_tcp_sender.php
+++ php/src/net_tcp_sender.php
@@ -1,5 +1,45 @@
 <?php
 
-echo "OK".PHP_EOL;
+set_time_limit(0);
+
+const RECEIVER_ADDRESS = "127.0.0.1";
+const RECEIVER_PORT = 10000;
+
+echo "TCP IPv4 stream socket sender".PHP_EOL;
+
+$socket = @socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+if ($socket === false)
+{
+  exit("- socket_create() failed: ".socket_strerror(socket_last_error()).PHP_EOL);
+}
+echo "- socket created".PHP_EOL;
+
+$result = @socket_connect($socket, RECEIVER_ADDRESS, RECEIVER_PORT);
+if ($result === false)
+{
+  exit("- socket_connect() failed: ".socket_strerror(socket_last_error($socket)).PHP_EOL);
+}
+echo "- connected to ".RECEIVER_ADDRESS.":".RECEIVER_PORT.PHP_EOL;
+
+if (isset($argv[1]))
+{
+  $msg = $argv[1]; // message is the first parameter, for example "stop" to stop the receiver
+}
+else
+{
+  $msg = "This is PHP message sent at ".date("j.n.Y")." in ".date("G:i:s");
+}
+echo "|".$msg."|".PHP_EOL;
+
+$msg_length = strlen($msg); 
+$sent_length = @socket_send($socket, $msg, $msg_length, 0);
+if ($sent_length === false)
+{
+  exit("- socket_send() failed: ".socket_strerror(socket_last_error($socket)).PHP_EOL);
+}
+echo "- message ".$sent_length."B of ".$msg_length."B sent".PHP_EOL;
+
+socket_close($socket);
+echo "- socket closed".PHP_EOL;
 
 ?>
